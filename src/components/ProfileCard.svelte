5c778635fea0f5cf447e95851e963fee3f09f833
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2021-07-02 00:36:26 -0400

    TODO: Refactor dom elements stucture in slides

--- src/components/ProfileCard.svelte
+++ src/components/ProfileCard.svelte
@@ -107,7 +107,7 @@
     filter: drop-shadow(0px 6px 8px rgba(0, 0, 0, 0.25));
   }
   .admin {
-    border: 5px solid #FFE503 !important;
+    border: 5px solid #FFE503;
   }
   .admin-crown {
     width: 120px;
